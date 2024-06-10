f3dfb62753451c947c4f20c6ec5cc1c1378e6ce5
stonedjjh <134118059+stonedjjh@users.noreply.github.com>
2024-06-10 19:55:21 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,29 @@
+function skillsMember() {
+  return [
+    {
+      name: 'HTML',
+      level: '80%'
+    },
+    {
+      name: 'CSS',
+      level: '70%'
+    },
+    {
+      name: 'Javascript',
+      level: '65%'
+    },
+    {
+      name: 'React',
+      level: '50%'
+    },
+    {
+      name: 'Node.js',
+      level: '40%'
+    },
+    {
+      name: 'MongoDB',
+      level: '30%'
+    }
+  ]
+}
+
